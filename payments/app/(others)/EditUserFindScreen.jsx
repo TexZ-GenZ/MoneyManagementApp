de4229f40d0d5d558af12bbb072a1d25fb914cae
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-08-20 18:28:09 +0530

    folder changes

--- payments/app/(others)/EditUserFindScreen.jsx
+++ payments/app/(others)/EditUserFindScreen.jsx
@@ -61,7 +61,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        justifyContent: "center",
+        //justifyContent: "center",
         backgroundColor: "#fff",
     },
     title: {
