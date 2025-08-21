ce559fe8acb8d5ea5b40ce54dd04d3caf1ef00ab
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-08-21 22:49:56 +0530

    notify screen skeleton code

---
+++ payments/app/(others)/NotifyAccountant.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotifyAccountant = () => {
+  return (
+    <View>
+      <Text>NotifyAccountant</Text>
+    </View>
+  )
+}
+
+export default NotifyAccountant
