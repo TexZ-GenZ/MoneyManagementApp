71af07c5835dfe4e2c0f200b1a0076a830a93ab5
TexZ-GenZ <priyanshusamal21@gmail.com>
2025-08-28 16:18:38 +0530

    ~29 mb

--- payments/babel.config.js
+++ payments/babel.config.js
@@ -1,16 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [
-    // Keep Reanimated plugin last
-      ];
+  const plugins = [];
 
   if (process.env.NODE_ENV === 'production') {
     // Drop console.* calls in release builds to reduce JS bundle size
-    plugins.unshift([
+    plugins.push([
       'transform-remove-console',
       { exclude: ['error', 'warn'] },
     ]);
   }
+
+  // Keep Reanimated plugin last
+  plugins.push('react-native-reanimated/plugin');
 
   return {
     presets: ['babel-preset-expo'],
--- payments/metro.config.js
+++ payments/metro.config.js
@@ -2,4 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Reduce startup time and JS memory footprint by deferring requires in production
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
 module.exports = config;
